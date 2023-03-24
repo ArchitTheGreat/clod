4e289a5f1424e609fcaf0ae40d9c4a34e9808844
aaaa9234 <86586285+ArchitTheGreat@users.noreply.github.com>
2023-03-24 13:13:24 +0530

    Add files via upload

---
+++ welcome-cb.py
@@ -0,0 +1,17 @@
+import random
+
+def chatbot():
+    # Define some responses
+    responses = [
+        "Hello!",
+        "How are you today?",
+        "What brings you here?",
+        "Nice to meet you!",
+        
+    ]
+    
+    # Print a random response
+    print(random.choice(responses))
+
+# Call the chatbot function
+chatbot()
